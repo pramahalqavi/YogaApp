2598fb0c826275d87b0334e8fe467b971795ae05
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-07-07 02:07:16 +0700

    finish splash screeng

--- app/src/main/java/com/android/paskahlis/yogaapp/MainActivity.java
+++ app/src/main/java/com/android/paskahlis/yogaapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.android.paskahlis.yogaapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.paskahlis.yogaapp.authorization.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(activity, LoginActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
