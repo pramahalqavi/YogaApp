a7f603c787479399c3d00a018e7f77f9cadd2ae8
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-07-07 02:53:49 +0700

    resolve app crashing cause

--- app/src/main/java/com/android/paskahlis/yogaapp/authorization/LoginActivity.java
+++ app/src/main/java/com/android/paskahlis/yogaapp/authorization/LoginActivity.java
@@ -15,7 +15,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
     }
 
     public void registerLabelOnClick(View view) {
--- app/src/main/java/com/android/paskahlis/yogaapp/authorization/RegistrationActivity.java
+++ app/src/main/java/com/android/paskahlis/yogaapp/authorization/RegistrationActivity.java
@@ -13,7 +13,6 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        getSupportActionBar().hide();
     }
 
     public void exitPendaftaranButtonOnClick(View view) {
