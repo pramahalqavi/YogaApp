3e08e950f784159a159b042ca51551b5172fc1ab
Confickr <pramahalqavi97@gmail.com>
2018-07-07 22:02:35 +0700

    add yoga alarm

--- app/src/main/java/com/android/paskahlis/yogaapp/activity/SetDateActivity.java
+++ app/src/main/java/com/android/paskahlis/yogaapp/activity/SetDateActivity.java
@@ -2,17 +2,33 @@ package com.android.paskahlis.yogaapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.paskahlis.yogaapp.R;
 import com.android.paskahlis.yogaapp.fragment.SetDateFragment;
 
 public class SetDateActivity extends AppCompatActivity {
+
+    public ImageView alarmButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_date);
+
+        alarmButton = findViewById(R.id.alarm_button);
+        alarmButton.setVisibility(View.INVISIBLE);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.date_container, new SetDateFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        alarmButton.setVisibility(View.INVISIBLE);
+        super.onBackPressed();
     }
 }
